53c66b0b5b23b58dcfe6ce47b05e649a2b5334a1
YuriyChob <sonicmaster172@gmail.com>
2020-04-12 23:40:30 +0300

    default-setup(0.1.0)

---
+++ src/config/webpack.prod.js
@@ -0,0 +1,39 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+// const { NODE_ENV = 'production' } = process.env;
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.ts'
+  },
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[name].bundle.js'
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: true,
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
+  },
+  resolve: { extensions: ['.js', '.ts', '.json', '.mjs'] },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      }
+    ]
+  }
+};

586b3e4963cab39013c08f30ff2ecf26f22549b7
YuriyChob <sonicmaster172@gmail.com>
2020-04-17 23:15:50 +0300

    default-setup(0.1.4) default security middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,50 @@
 import express from 'express';
 import morgan from 'morgan';
 
+// Security
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+
 import appConfig from './config';
 
 const app = express();
+
+// Security http headers
+// should be one of the first
+app.use(helmet());
 
 if (appConfig.environment === 'development') {
   app.use(morgan('short'));
 }
 
+// Limit requests from same ip
+// Numbers are not for production
 app.use('/', (req, res, next) => {
   res.status(200).send('All is good');
 });
+
+app.use(
+  '/api',
+  rateLimit({
+    max: 1000,
+    windowMs: 3600 * 1000,
+    message: 'too many request from this IP',
+  }),
+);
+
+// write http body data to req.body
+app.use(
+  express.json({
+    limit: '1mb',
+  }),
+);
+
+// Prevent parameters pollutions
+app.use(hpp());
+
+// Data sanitization against noSQL injections
+app.use(mongoSanitize());
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,6 @@ import app from './app';
 import appConfig from './config';
 
 app.listen(appConfig.port, () => {
+  // tslint:disable-next-line: no-console
   console.log(`App is running at port ${appConfig.port}`);
 });

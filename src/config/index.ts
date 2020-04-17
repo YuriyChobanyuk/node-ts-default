054f34284950e0541d3057e7844dfa2590d9402b
YuriyChob <sonicmaster172@gmail.com>
2020-04-17 23:15:50 +0300

    default-setup(0.1.4) default security middleware

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,17 @@ import { config } from 'dotenv';
 config();
 
 const appConfig = {
-  port: process.env.PORT || 3000,
-  environment: process.env.NODE_ENV,
+  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || '',
+  MONGODB_USERNAME: process.env.MONGODB_USERNAME || 'defaultUser',
+  PORT: process.env.PORT || 4000,
+  JWT_SECRET: process.env.JWT_SECRET || 'default_secret_key',
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1h',
+  JWT_EXPIRES_IN_COOKIE: process.env.JWT_EXPIRES_IN_COOKIE || 3600000,
+  EMAIL_NAME: process.env.EMAIL_NAME,
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  NODE_ENV: process.env.NODE_ENV,
 };
 
 export default appConfig;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,32 @@
-import app from './app';
+import mongoose from 'mongoose';
+
 import appConfig from './config';
 
-app.listen(appConfig.port, () => {
-  console.log(`App is running at port ${appConfig.port}`);
+import app from './app';
+
+const server = app.listen(appConfig.PORT, () => {
+  // tslint:disable-next-line: no-console
+  console.log(`App is running at port ${appConfig.PORT}`);
 });
+
+app.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION');
+  console.error(err);
+
+  server.close(() => {
+    process.exit(0);
+  });
+});
+
+const DB = `mongodb+srv://${appConfig.MONGODB_USERNAME}:${
+  appConfig.MONGODB_PASSWORD
+}@cluster0-jgzf0.mongodb.net/naturous?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
